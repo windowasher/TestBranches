bb27fd09a78e3d0eac2a821a86bc7fc345c337e7
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2018-07-05 13:17:00 -0400

    First Checking

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Branches
+
+Maven JUNIT test package for grader API, contains passing and failing test only
---
+++ src/test/java/io/zipcoder/TestWithPassAssertions.java
@@ -0,0 +1,31 @@
+package io.zipcoder;
+
+import org.junit.Test;
+
+public class TestWithPassAssertions {
+
+    @Test
+    public void test1(){
+
+    }
+
+    @Test
+    public void test2(){
+
+    }
+
+    @Test
+    public void test3(){
+
+    }
+
+    @Test
+    public void test4(){
+
+    }
+
+    @Test
+    public void test5(){
+
+    }
+}
