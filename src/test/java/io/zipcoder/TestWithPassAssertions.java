ee4cb23318c9d00666fa9c499763c77777603edd
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2018-07-05 13:19:31 -0400

    All fail

--- src/test/java/io/zipcoder/TestWithPassAssertions.java
+++ src/test/java/io/zipcoder/TestWithPassAssertions.java
@@ -1,36 +1,37 @@
 package io.zipcoder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestWithPassAssertions {
 
     @Test
     public void test1(){
-
+        Assert.assertTrue(false);
     }
 
     @Test
     public void test2(){
-
+        Assert.assertTrue(false);
     }
 
     @Test
     public void test3(){
-
+        Assert.assertTrue(false);
     }
 
     @Test
     public void test4(){
-
+        Assert.assertTrue(false);
     }
 
     @Test
     public void test5(){
-
+        Assert.assertTrue(false);
     }
 
     @Test
     public void test6(){
-        
+        Assert.assertTrue(false);
     }
 }
